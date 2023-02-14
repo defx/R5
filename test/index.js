96db4327054c4967aa773a6834e6ef31a950d108
defx <mdonkin83@gmail.com>
2023-02-14 14:37:38 +0000

    initial tests

--- test/index.js
+++ test/index.js
@@ -1,13 +1,5 @@
 import { construct, html } from "../src/index.js"
 
-function reformat(str) {
-  return str.replace(/\n|\s{2,}/gm, "")
-}
-
-function stripComments(str) {
-  return str.replace(/<!--[\s\S]*?-->/gm, "")
-}
-
 describe("construct", () => {
   let rootNode
 
@@ -20,8 +12,40 @@ describe("construct", () => {
     document.body.removeChild(rootNode)
   })
 
-  it("...", () => {
-    construct(rootNode, () => html`<p>hi</p>`)()
-    assert.equal(rootNode.innerHTML, "<p>hi</p>")
+  it("renders static content", () => {
+    construct(
+      rootNode,
+      () =>
+        html`<!-- hello world! -->
+          <p>hi</p>`
+    )()
+
+    assert.equal(rootNode.innerHTML, "<!-- hello world! --><p>hi</p>")
+  })
+
+  it("sets text", () => {
+    const render = construct(rootNode, (value) => html`<p>${value}</p>`)
+
+    render(0)
+    assert.equal(rootNode.children[0].textContent, "0")
+  })
+
+  it("reuses nodes", () => {
+    const render = construct(rootNode, (value) => html`<p>${value}</p>`)
+
+    render(0)
+    const p = rootNode.children[0]
+    render(1)
+    assert.equal(rootNode.children[0], p)
+  })
+
+  it("sets attributes", () => {
+    const render = construct(
+      rootNode,
+      ({ one, two }) => html`<p class="${one} ${two}"></p>`
+    )
+
+    render({ one: "foo", two: "bar" })
+    assert(rootNode.children[0].getAttribute("class"), "foo bar")
   })
 })
