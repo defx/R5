b35a9a7ba7f5cffde31fb0f158c9247153bfa33c
defx <mdonkin83@gmail.com>
2023-02-15 20:46:17 +0000

    ...

--- test/parse.js
+++ test/parse.js
@@ -14,24 +14,46 @@ const parts = [
 
 function parse(str) {
   const m = {}
-  const parts = str.split(/{{(\d)}}/gm)
-  //   console.log(parts)
-  parts.forEach((x, i) => {
-    if (isNaN(x)) return
+  const chars = str.split("")
 
-    const n = +x
-    const prev = parts[i - 1]
-    const type = prev.match(/\s[\w]+=["']$/) ? "ATTRIBUTE" : "TEXT"
+  let t
 
-    console.log(n, prev, type)
+  chars.forEach((c, i) => {
+    let prev = chars[i - 1]
+    let next = chars[i + 1]
+
+    if (c === `<`) {
+      if (next === `/`) {
+        console.log("start close tag")
+        t = 0
+      } else {
+        console.log("start open tag")
+        t = 1
+      }
+    }
+    if (c === `>`) {
+      if (t) {
+        console.log("end open tag")
+      } else {
+        console.log("end close tag")
+      }
+    }
+    if (c.match(/['"]/)) {
+      if (prev === `=`) {
+        console.log("open attribute")
+      } else {
+        console.log("close attribute")
+      }
+    }
   })
+
   // ...
   return { m }
 }
 
 describe("parse", () => {
   it("...", () => {
-    const html = `<section><p class="{{0}}">{{1}}</p>{{2}}<a onclick='{{3}}'>{{4}}and{{5}}</a></section><span>{{6}}</span>`
+    const html = `<section><p class="{{0}} foo">{{1}}and{{2}}</p>{{3}}<a onclick='{{4}}'>{{5}}and{{6}}</a></section><span>{{7}}</span>`
 
     const { m } = parse(html)
   })
