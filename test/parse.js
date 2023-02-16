f9b50596d2cb3576dd89ff880ff839bbf10eeeff
defx <mdonkin83@gmail.com>
2023-02-16 17:56:01 +0000

    ...

--- test/parse.js
+++ test/parse.js
@@ -23,10 +23,24 @@ don't over-egg it, all we need is...
 
 1. each attribute: (nodeIndex, name, value)
 2. each text node: (nodeIndex, value)
+
+
+in terms of the node index...
+
+i think you can just count
+
+    - tag open
+    - start text
   
   */
 
   let t
+
+  let e = 0
+
+  let text = ""
+  let attr = ""
+  let attrName = ""
 
   chars.forEach((c, i) => {
     let prev = chars[i - 1]
@@ -34,28 +48,49 @@ don't over-egg it, all we need is...
 
     if (c === `<`) {
       if (next === `/`) {
-        console.log("start close tag")
+        console.log("start close tag", { text })
         t = 0
+        e = 1
+        // @todo: emit text
+        text = ""
       } else {
-        console.log("start open tag")
+        console.log("start open tag", { text })
         t = 1
+        e = 1
+        // @todo: emit text
+
+        text = ""
       }
     } else if (c === `>`) {
       if (t == 1) {
         console.log("end open tag")
+        e = 0
       }
       if (t === 0) {
         console.log("end close tag")
+        e = 0
       }
     } else if (c.match(/['"]/)) {
       if (prev === `=`) {
+        attrName = str
+          .slice(0, i - 1)
+          .split(" ")
+          .pop()
+
         console.log("open attribute")
         t = 2
       } else if (t === 2) {
-        console.log("close attribute")
+        console.log("close attribute", { name: attrName, value: attr })
         // @todo: emit attribute
+        attr = ""
+        attrName = ""
         t = 1
       }
+    } else if (e === 0) {
+      text += c
+      console.log("text", c)
+    } else if (t === 2) {
+      attr += c
     }
   })
 
