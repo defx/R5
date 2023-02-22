3b9d81c3d5316bf76a4606b4a35d4051919e64a3
defx <mdonkin83@gmail.com>
2023-02-22 12:03:37 +0000

    ...

--- src/template.js
+++ src/template.js
@@ -1,22 +1,10 @@
-import { ATTRIBUTE, DYNAMIC, EMPTY, EVENT, STATIC } from "./constants.js"
-import { hasMustache, getParts } from "./token.js"
-import { last, walk } from "./helpers.js"
 import { parse } from "./parse.js"
-import * as Placeholder from "./placeholder.js"
-
-function withPlaceholders(strings) {
-  return (
-    strings.slice(0, -1).reduce((a, s, i) => {
-      return a + s + `{{${i}}}`
-    }, "") + last(strings)
-  ).trim()
-}
 
 const cache = new Map()
 
 export function html(strings, ...values) {
   // @todo: replace with a simple(r) join
-  const key = withPlaceholders(strings)
+  const key = strings.join("%")
 
   if (!cache.has(key)) {
     cache.set(key, parse(strings))
@@ -30,92 +18,5 @@ export function html(strings, ...values) {
       this.$key = k
       return this
     },
-  }
-}
-
-function xparse(str) {
-  const map = {}
-
-  let k = -1
-
-  const template = document.createElement("template")
-  template.innerHTML = str.replace(/\n\s*/gm, "")
-
-  walk(template.content.firstChild, (node) => {
-    k += 1
-
-    if (node.nodeType === Node.TEXT_NODE) {
-      const { textContent } = node
-      if (!hasMustache(textContent)) {
-        return
-      }
-
-      /*
-      
-      the following code replaces all of the dynamic parts of text nodes with comment placeholders....which can all be done without DOM. 
-      
-      */
-
-      const parts = getParts(textContent)
-
-      const frag = document.createDocumentFragment()
-
-      let k2 = k
-
-      for (const [i, part] of Object.entries(parts)) {
-        k = k2 + +i
-
-        if (part.type === STATIC) {
-          const text = document.createTextNode(part.value)
-          frag.appendChild(text)
-        }
-        if (part.type === DYNAMIC) {
-          const placeholder = Placeholder.create(EMPTY)
-          frag.appendChild(placeholder)
-
-          map[k] = map[k] || []
-          map[k].push({
-            index: part.index,
-          })
-        }
-      }
-
-      const nextSibling = node.nextSibling || node.parentNode.nextSibling
-
-      node.parentNode.replaceChild(frag, node)
-
-      return nextSibling
-    }
-
-    if (node.nodeType === Node.ELEMENT_NODE) {
-      let attrs = [...node.attributes]
-      let i = attrs.length
-
-      while (i--) {
-        let { name, value } = attrs[i]
-
-        if (!hasMustache(value)) return
-
-        const parts = getParts(value)
-
-        map[k] = map[k] || []
-        map[k].push(
-          name.startsWith("on")
-            ? { type: EVENT, name: name.slice(2), index: parts[0].index }
-            : {
-                type: ATTRIBUTE,
-                name,
-                parts,
-              }
-        )
-
-        node.removeAttribute(name)
-      }
-    }
-  })
-
-  return {
-    m: map,
-    t: template.innerHTML,
   }
 }
