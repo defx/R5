5ce2b3e9273913407df0e9e76f7f35b964eb9cde
defx <mdonkin83@gmail.com>
2023-03-03 21:18:14 +0000

    get existing block nodes

--- src/update.js
+++ src/update.js
@@ -1,20 +1,27 @@
 import { last, walk } from "./helpers.js"
 
-function next(node, { before, after, value }) {
-  const isAttr = before.match(/(\w+-?\w+)=['"]{1}([^'"]*)$/)
-
-  if (isAttr) {
-    // ...
-  } else {
-    // ...
-  }
-
-  console.log("next", node, node.nextSibling, { isAttr, before, after, value })
-}
-
 const isTemplateResult = (v) => v?.hasOwnProperty("markup")
 
 const isPrimitive = (v) => v === null || typeof v !== "object"
+
+const isOpenBrace = (node) =>
+  node.nodeType === Node.COMMENT_NODE && node.textContent === "{"
+
+const isCloseBrace = (node) =>
+  node.nodeType === Node.COMMENT_NODE && node.textContent === "}"
+
+const getBlockNodes = (sentinel) => {
+  let nodes = []
+  walk(
+    sentinel.nextSibling,
+    (n) => {
+      if (isCloseBrace(n)) return null
+      nodes.push(n)
+    },
+    false
+  )
+  return nodes
+}
 
 export const update = (templateResult, rootNode) => {
   const { markup, strings, values } = templateResult
@@ -24,25 +31,24 @@ export const update = (templateResult, rootNode) => {
   let i = 0
 
   walk(rootNode, (node) => {
-    if (node.nodeType === Node.COMMENT_NODE) {
-      const isOpenBrace = node.textContent === "{"
+    if (isOpenBrace(node)) {
+      const { nextSibling } = node
+      const value = values[i++]
 
-      if (isOpenBrace) {
-        const { nextSibling } = node
-        const value = values[i++]
-
-        if (isPrimitive(value)) {
-          if (nextSibling.nodeType === Node.TEXT_NODE) {
-            if (nextSibling.textContent !== value) {
-              nextSibling.textContent = value
-            }
+      if (isPrimitive(value)) {
+        if (nextSibling.nodeType === Node.TEXT_NODE) {
+          if (nextSibling.textContent !== value) {
+            nextSibling.textContent = value
           }
-
-          return nextSibling
-        } else if (Array.isArray(value) && isTemplateResult(value[0])) {
-          // this is followed by a repeated block...
-          // @todo: grab all the nodes between this node and the next closing brace
         }
+
+        return nextSibling
+      } else if (Array.isArray(value) && isTemplateResult(value[0])) {
+        // this is followed by a repeated block...
+        // @todo: grab all the nodes between this node and the next closing brace
+        let blockNodes = getBlockNodes(node)
+
+        console.log({ blockNodes })
       }
 
       //   const stars = node.textContent.match(/(\*+)/)?.[1].split("")
